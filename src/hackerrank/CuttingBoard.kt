5b885a46f18585b607b4fcda7d0da3c95725264c
Artem Zhdanov <temas_coder@yahoo.com>
2016-05-15 21:49:52 -0700

    fix

--- src/hackerrank/CuttingBoard.kt
+++ src/hackerrank/CuttingBoard.kt
@@ -12,46 +12,39 @@ import java.io.FileWriter
  */
 
 fun main(args: Array<String>) {
-    CuttingBoard.start(args)
-}
-
-object CuttingBoard {
-
     val IN = "res/hackerrank/cutboard/sample.in"
     val OU = "res/hackerrank/cutboard/sample.out"
 
-    fun start(args: Array<String>) {
-        val br = BufferedReader(FileReader(IN))
-        val fw = BufferedWriter(FileWriter(OU))
+    val br = BufferedReader(FileReader(IN))
+    val fw = BufferedWriter(FileWriter(OU))
 
-        var line = br.readLine()
-        val numCases = Integer.valueOf(line)
+    var line = br.readLine()
+    val numCases = Integer.valueOf(line)
 
-        for (i in 1..numCases) {
-            var result : Long = 0
-            val mn = br.readLine().split(" ")
-            val m = mn[0].toInt()
-            val n = mn[1].toInt()
-            val ma = br.readLine().split(" ").map { it.toInt() }
-            val na = br.readLine().split(" ").map { it.toInt() }
+    for (i in 1..numCases) {
+        var result : Long = 0
+        val mn = br.readLine().split(" ")
+        val m = mn[0].toInt()
+        val n = mn[1].toInt()
+        val ma = br.readLine().split(" ").map { it.toInt() }
+        val na = br.readLine().split(" ").map { it.toInt() }
 
-            val list = ma.mapIndexed { idx, v -> Triple(idx, v, 1) }.
-                    plus(na.mapIndexed { idx, v -> Triple(idx, v, 0) }).
-                    sortedByDescending { it.second + it.third}
-            var mc = 1
-            var nc = 1
-            list.forEach {
-                if (it.second == 0) {
-                    result += it.first * nc
-                    mc++
-                } else {
-                    result += it.first * mc
-                    nc++
-                }
+        val list = ma.mapIndexed { idx, v -> Triple(idx, v, 1) }.
+                plus(na.mapIndexed { idx, v -> Triple(idx, v, 0) }).
+                sortedByDescending { it.second + it.third}
+        var mc = 1
+        var nc = 1
+        list.forEach {
+            if (it.third == 0) {
+                result += it.second * nc
+                mc++
+            } else {
+                result += it.second * mc
+                nc++
             }
-            fw.write(result.toString())
         }
-        fw.flush()
-        fw.close()
+        fw.write(result.toString() + "\n")
     }
+    fw.flush()
+    fw.close()
 }
