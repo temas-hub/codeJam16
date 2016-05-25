76ab5a398b4461672108285653bd6da065755afe
Artem Zhdanov <artem.zhdanov@macys.com>
2016-05-24 18:05:21 -0700

    t3 template

--- src/r1/t3/T3.kt
+++ src/r1/t3/T3.kt
@@ -18,29 +18,24 @@ import java.util.*
  * Date: 04.05.13
  */
 
-val IN = "res/r1/t2/B-large-practice.in"
-val OU = "res/r1/t2/B-small-practice.out"
 
-//val IN = "res/r1/t2/sample.in"
-//val OU = "res/r1/t2/sample.out"
+fun main(args: Array<String>) {
 
-object T3 {
-    @Throws(Exception::class)
-    @JvmStatic fun main(args: Array<String>) {
+    val IN = "res/r1/t3/sample.in"
+    val OU = "res/r1/t3/sample.out"
 
-        val br = BufferedReader(FileReader(IN))
-        val fw = BufferedWriter(FileWriter(OU))
+    val br = BufferedReader(FileReader(IN))
+    val fw = BufferedWriter(FileWriter(OU))
 
-        var line = br.readLine()
-        val numCases = Integer.valueOf(line)
+    var line = br.readLine()
+    val numCases = Integer.valueOf(line)
 
-        for (i in 1..numCases) {
-            val inp = br.readLine()
+    for (i in 1..numCases) {
+        val inp = br.readLine()
 
 //            fw.write(String.format("Case #%d: %s %s\n", i, can1, can2))
-            println(i)
-        }
-        fw.flush()
-        fw.close()
+        println(i)
     }
+    fw.flush()
+    fw.close()
 }
